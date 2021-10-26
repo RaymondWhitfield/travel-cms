35f7d57c5f5d3811d3a7fb265d8373100e03a250
radnee <75645533+radnee@users.noreply.github.com>
2021-10-25 19:45:12 -0500

    Add files via upload

---
+++ Confirmation.php
@@ -0,0 +1,59 @@
+<?php
+	$confirmationId = "";
+
+	if (isset($_GET["confirmationId"])) {
+		$confirmationId = $_GET["confirmationId"];
+	}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Polar Lights | Confirmation Page</title>
+	<!-- Linking Bootstrap CSS file -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+	<style>@import url('https://fonts.googleapis.com/css2?family=Sora:wght@300&display=swap');</style>
+	<style>@import url('https://fonts.googleapis.com/css2?family=Oleo+Script&display=swap');</style>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- Linking Polar Lights project specific CSS file -->
+	<link id = "style" rel="stylesheet" href="style.css">
+	<link id="active-stylesheet" href="" rel="stylesheet" type="text/css"/>
+	
+	
+</head>
+<body>
+	
+	<!-- Navigation bar using Bootstrap's in-built navigation section -->
+	<?php
+		include_once "navbar.php";
+	?>
+
+	<!-- Container for main body -->
+	<div class="container container-body contact-section">
+		<!-- Div to center the content and bottom padding using Bootstrap class -->
+		<div class="row justify-content-center pb-5 mb-5">
+			<!-- Div for medium column using Bootstrap class -->
+			<div class="col-md-7 text-justify">
+				<!-- Header with top and bottom padding using Bootstrap class -->
+				<h2 class="p-2 subTitles subTitlesFont">Confirmation</h2>
+
+				<p class="pt-3">Your booking confirmation number is <?php echo $confirmationId ?>.</p>
+
+				<p>Enjoy for trip!!</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Page footer -->
+	<?php
+		include_once "footer.php";
+	?>
+	
+	<!-- Linking Polar Lights project specific JavaScript file -->
+	<script src="script.js"></script>
+</body>
+</html>
