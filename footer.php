7916486ae0ff4735e2adb26deb673196329cc399
radnee <75645533+radnee@users.noreply.github.com>
2021-11-13 09:31:23 -0600

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,21 +1,18 @@
 <!-- 
-		* Page footer 
+	* Page footer 
+-->
+<footer>
+	<!-- 
+	* Div with top and bottom padding as well as centered text with Bootstrap class 
 	-->
-	<footer>
+	<div class="pt-4 pb-2 text-center">
+		<h4>About <span><a href="index.php">Polar Lights</a></span></h4>
+		<p>Like we're made of starlight!</p>
 		<!-- 
-		* Div with top and bottom padding as well as centered text with Bootstrap class 
+			* Using JavaScript getFullYear function to get the current year from the Date function
 		-->
-		<div class="pt-4 pb-2 text-center">
-			<h4>About <span><a href="index.php">Polar Lights</a></span></h4>
-			<p>Like we're made of starlight!</p>
-			<p>
-				<a href="mailto:rdighe@edgewood.edu"><span>rdighe@edgewood.edu</span></a>
-			</p>
-			<!-- 
-				* Using JavaScript getFullYear function to get the current year from the Date function
-			-->
-			<p>
-				Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Created by Radnee Dighe
-			</p>
-		</div>
-	</footer>
+		<p>
+			Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Created by R & R
+		</p>
+	</div>
+</footer>
