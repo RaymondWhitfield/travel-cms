02c7b41551d8c2c8e6fed9c715e56b9bafa6732a
radnee <75645533+radnee@users.noreply.github.com>
2021-11-13 09:43:41 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,31 +1,39 @@
+<?php
+	/*
+	 * The include statement takes all the variables from the init.php file and copies it into this file
+	 * We do this so that the database configuration variables are defined only in one file
+	 */
+	include "init.php";
+
+	/*
+	 * Store current page
+	 */
+	$_SESSION["currentPage"]="index.php";
+?>
 
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<title>Polar Lights | Home Page</title>
-		<!-- 
-			* Linking Bootstrap CSS file 
-		-->
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+	<!-- 
+		* Linking Bootstrap CSS file 
+	-->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-		<!--
-			* Importing Google fonts
-		-->
-		<style>@import url('https://fonts.googleapis.com/css2?family=Sora:wght@300&display=swap');</style>
-		<style>@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap');</style>
-		<style>@import url('https://fonts.googleapis.com/css2?family=Oleo+Script&display=swap');</style>
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<!-- 
-			* Linking Polar Lights project specific CSS file 
-		-->
-		<!-- <link rel="stylesheet" href="style.css" type="text/css"> -->
-		<link id="active-stylesheet" href="" rel="stylesheet" type="text/css"/>
-
-		
-		
-	
+	<!--
+		* Importing Google fonts
+	-->
+	<style>@import url('https://fonts.googleapis.com/css2?family=Sora:wght@300&display=swap');</style>
+	<style>@import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@500&display=swap');</style>
+	<style>@import url('https://fonts.googleapis.com/css2?family=Oleo+Script&display=swap');</style>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<!-- 
+		* Linking Polar Lights project specific CSS file 
+	-->
+	<link rel="stylesheet" href="style.css" type="text/css">
+	<link id="active-stylesheet" href="" rel="stylesheet" type="text/css"/>
 </head>
 <body>	
 
@@ -222,9 +230,9 @@
 		include_once "footer.php";
 	?>
 
-<!-- 
-			* Linking Polar Lights project specific JavaScript file 
-		-->
+	<!-- 
+		* Linking Polar Lights project specific JavaScript file 
+	-->
 	<script src="script.js"></script>
 </body>
 </html>
