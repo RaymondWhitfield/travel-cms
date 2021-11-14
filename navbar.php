fd8b50fbbf029fbba17c57524ed61419a3f0d642
unknown <raymond.b.whitfield@gmail.com>
2021-11-13 18:39:13 -0600

    Updated navbar hamburger style

--- navbar.php
+++ navbar.php
@@ -1,8 +1,7 @@
-
 <!-- 
 		* Navigation bar using Bootstrap's in-built navigation section 
 	-->
-	<nav class = "navbar navbar-expand-lg navbar-dark bg-dark">
+	<nav class = "navbar navbar-collapse navbar-dark bg-dark">
 		<div class = "container-fluid">
 			<!-- 
 				* Heading for website 
@@ -12,7 +11,7 @@
 				
 				if(isset($_SESSION["FirstName"])){
 					$name = $_SESSION["FirstName"];
-					echo "<li>Welcome back, $name </li>";
+					echo "<li id='welcomemsg'>Welcome back, $name </li>";
 				}
 			?>
 			<div>
@@ -20,39 +19,34 @@
 					* Navigation menu as unordered list, each list item has specific padding using Bootstrap class 
 				-->
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				  </button>
+					<span class="navbar-toggler-icon"></span></button>
+					
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav">
-					<li><a href="index.php" class="nav-link"><span>HOME</span></a></li>
+					<li class="nav-item"><a class="nav-link active" aria-current="page" href="index.php"><span>HOME</span></a></li>
 					
-				
-					<li>
-						<div class="dropdownDiv">
-							<button class="dropdownBtn nav-link"><span>CONTINENTS</span>
-								<i class="fa fa-caret-down"></i>
-							</button>
-							<div class="dropdown-content">
-								<a href="Antarctica.php" class="dropdown-item"><span>ANTARCTICA</span></a>
-								<a href="Europe.php" class="dropdown-item"><span>EUROPE</span></a>
-								<a href="NorthAmerica.php" class="dropdown-item"><span>NORTH AMERICA</span></a>
-								<a href="Oceania.php" class="dropdown-item"><span>OCEANIA</span></a>
-								<a href="SouthAmerica.php" class="dropdown-item"><span>SOUTH AMERICA</span></a>
-							</div>
-						</div>
-					</li>
+						<li class="nav-item dropdown">
+							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"><span>CONTINENTS</span></a>
+							<ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+								<li><a class="dropdown-item" href="Antarctica.php"><span>ANTARCTICA</span></a></li>
+								<li><a class="dropdown-item" href="Europe.php"><span>EUROPE</span></a></li>
+								<li><a class="dropdown-item" href="NorthAmerica.php"><span>NORTH AMERICA</span></a></li>
+								<li><a class="dropdown-item" href="Oceania.php"><span>OCEANIA</span></a></li>
+								<li><a class="dropdown-item" href="SouthAmerica.php"><span>SOUTH AMERICA</span></a></li>
+							</ul>
+						</li>
 					<li><a href="Gallery.php" class="nav-link"><span>GALLERY</span></a></li>
 					<li><a href="Search.php" class="nav-link"><span>SEARCH</span></a></li>
 					<li><a href="Reviews.php"class="nav-link"><span>REVIEWS</span></a></li>
 					<li><a href="About.php" class="nav-link"><span>ABOUT</span></a></li>
 					<?php
 						if(isset($_SESSION["Email"])){
-							echo "<li><a href='user.php' class='nav-link'><span>ACCOUNT</span></a></li>";
-							echo "<li><a href='logout.php' class='nav-link'><span>LOGOUT</span></a></li>";
+							echo "<li class='nav-item'><a href='user.php' class='nav-link'><span>ACCOUNT</span></a></li>";
+							echo "<li class='nav-item'><a href='logout.php' class='nav-link'><span>LOGOUT</span></a></li>";
 						}
 						else{
-							echo "<li><a href='register.php' class='nav-link'><span>REGISTER</span></a></li>";
-							echo "<li><a href='login.php' class='nav-link'><span>LOGIN</span></a></li>";
+							echo "<li class='nav-item'><a href='register.php' class='nav-link'><span>REGISTER</span></a></li>";
+							echo "<li class='nav-item'><a href='login.php' class='nav-link'><span>LOGIN</span></a></li>";
 						}
 					?>
 					<li><a class="nav-link changeCSS" data-stylesheet="darkMode.css"><span>DARK</span></a></li>
